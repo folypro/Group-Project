4c66925ad38d2d8e6f071f8f524b7465e658fa7c
Abdulbasit Adeniji <aadeniji@carrollcc.edu>
2022-04-24 03:33:26 -0400

    added my java class

---
+++ Abdulbasit.java
@@ -0,0 +1,8 @@
+public class Abdulbasit {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Team!" +
+                "I'm Abdulbasit Adeniji!" +
+                "\n Project idea are a billing or management system! Example, School billing system or store management system!");
+    }
+}
