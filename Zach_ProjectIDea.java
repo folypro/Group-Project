a05abba806d65caf8f55031fa699e1508ef83dce
ZachBowman721 <zbowman@carrollcc.edu>
2022-04-24 11:26:27 -0400

    I have updated our project with my idea of what to do.

--- Zach_ProjectIDea.java
+++ Zach_ProjectIDea.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class Zach_ProjectIDea
 {
     public static void main(String[] args)
@@ -8,3 +9,15 @@ public class Zach_ProjectIDea
 
     }
 }
+=======
+public class Zach_ProjectIDea
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello team, my name is Zach Bowman. " +
+                "\nI think a mortgage calculator would be neat, since I am looking for houses to buy.");
+
+    }
+}
+>>>>>>> 17ac797 (I have updated our project with my idea of what to do.)
