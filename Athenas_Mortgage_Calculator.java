1056362f78ce2320546fbcbf7c554ea38b868518
ZachBowman721 <zbowman@carrollcc.edu>
2022-05-02 10:49:52 -0400

    Athena's Mortgage Calculator. Please add more information to this.

---
+++ Athenas_Mortgage_Calculator.java
@@ -0,0 +1,50 @@
+// Zach Bowman
+// Started 05/02/2022
+// Athena's Mortgage Calculator
+// Description: This program will take the user's information, loan amount, loan term, loan rate.
+// It will then display back to the user information on the loan and how much they will be paying monthly.
+
+// Zach Bowman -- For other in the team, I have started getting the outline of our project together.
+// I will commit this to github so everyone can add their touches and part to it before it is due.
+// Please make sure to help with the outline of our project or add something.
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Athenas_Mortgage_Calculator extends Application
+{
+
+    // Global Variables
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+        // Labels
+
+        //Text Fields
+
+        // Button to convert the information
+
+        // HBox or VBox information
+
+        // alignment if needed
+
+        // padding
+
+        // Scene
+
+        // Stage
+
+        // Title the stage
+
+        // Show window
+
+    }
+}
