5dca8107938d03c71f0499af5ee2f9b57ec6032b
ZachBowman721 <zbowman@carrollcc.edu>
2022-05-03 09:52:48 -0400

    I added Labels and Fields, please look it over and add more.

--- Athenas_Mortgage_Calculator.java
+++ Athenas_Mortgage_Calculator.java
@@ -11,10 +11,31 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Athenas_Mortgage_Calculator extends Application
 {
 
     // Global Variables
+
+    // Fields
+    private TextField
+                        users_name, // Users input for his name.
+
+                        users_loan_amount, // Users input for the loan amount.
+
+                        users_loan_years, // Users input for the loan amount in years.
+
+                        users_interest_rate; // Users input for the interest rate.
+
+    //Labels
+    private Label
+                        text_field_total_amount_paid, // Empty field to hold the total amount paid.
+
+                        text_field_interest_paid, // Empty field to hold the interest only paid.
+
+                        text_field_monthly_mortgage; // Empty field to hold the monthly mortgage.
+
 
 
     public static void main(String[] args)
@@ -27,6 +48,15 @@ public class Athenas_Mortgage_Calculator extends Application
     {
 
         // Labels
+        Label
+                loan_amount_prompt = new Label("Please enter the loan amount for your mortgage:"), // Will be the description to get the users information for loan amount
+
+                loan_years_prompt = new Label("Please enter the years your loan is for:"), // Will be the description to get the users years of the loan
+
+                loan_interest_rate_prompt = new Label("Please enter the interest rate for your loan:"), // Will be the description to get the interest rate from user.
+
+                mortgage_calc_description = new Label("Welcome to Athena's Mortgage Calculator." +
+                        " Enter all required information for your monthly payment."); // Will be the description of the application to the user.
 
         //Text Fields
 
