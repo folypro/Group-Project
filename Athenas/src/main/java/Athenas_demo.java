9ad095ab1dedd1525f59c5f412e7a9e67049fc21
Angell1509 <aangell@carrollcc.edu>
2022-05-04 16:46:35 -0400

    PLEASE GET IN

--- Athenas/src/main/java/Athenas_demo.java
+++ Athenas/src/main/java/Athenas_demo.java
@@ -12,7 +12,7 @@ package Athenas.src.main.java;
 
 //Abdulbasit Adeniji -- I will be working on the input section for the user on 5/2/2022
 
-//Andrew Angell -- i will be working on the Calcbutton section for the user on 5/4/2022
+//Andrew Angell -- i will be working on the Calcbutton section for the user on 5/4/2022.
 
 
 import javafx.application.Application;
